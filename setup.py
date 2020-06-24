3e0756fe57810e2ffba3925e9504f0e765b4a545
Abhishek Sarkar <aksarkar@alum.mit.edu>
2020-06-24 13:38:49 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import setuptools
 
 setuptools.setup(
-  name=_name,
+  name='mpebpm',
   description='Massively Parallel Empirical Bayes Poisson Means',
   version='0.2',
   url='https://www.github.com/aksarkar/mpebpm',
   author='Abhishek Sarkar',
   author_email='aksarkar@uchicago.edu',
   license='MIT',
-  install_requires=[],
+  install_requires=['numpy', 'scipy', 'torch'],
   packages=setuptools.find_packages('src'),
   package_dir={'': 'src'},
+  tests_require=['pytest'],
 )
